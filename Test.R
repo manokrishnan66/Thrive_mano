315a7a87ad3ddc11c8bfa6d0991cffdfcc450691
Mano <manokrishnan66@hotmail.com>
2019-10-02 18:22:20 +0530

    Data import test

--- Test.R
+++ Test.R
@@ -1,3 +1,6 @@
+getwd()
+dir <-"/R_Projects/Thrive_mano/Projects" 
+setwd(dir)
 # ---Importing a datafile from dslabs
 
 p <- system.file("extdata", package="dslabs")
@@ -11,3 +14,34 @@ fullpath
 #-- copying to working directory
 file.copy(fullpath,getwd())
 file.exists(filename)
+
+#---read files
+library(tidyverse)
+library(readxl)
+read_line("murders.csv",n_max=3)
+'??readlines'
+dat <- read.csv(fullpath,stringsAsFactors = FALSE)
+class(dat$state)
+
+#-----Download from Internet
+url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
+dat <- read_csv(url)
+download.file(url, "murders.csv")
+tempfile()
+tmp_filename <- tempfile()
+download.file(url, tmp_filename)
+dat <- read_csv(tmp_filename)
+file.remove(tmp_filename)
+
+#---test
+library(tidyverse)
+install.packages("dplyr")
+library(dplyr)
+urlt <- "http://mlr.cs.umass.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
+datat <- read_csv(urlt,col_names = FALSE)
+str(datat)
+
+url <- "ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_annmean_mlo.txt"
+co2_mauna_loa <- read_table(url,col_names = TRUE,skip = 56)
+head(co2_mauna_loa)
+nrow(co2_mauna_loa %>% select(year, mean, unc))
