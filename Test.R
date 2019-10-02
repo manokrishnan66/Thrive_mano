2f0308c2ac6596ca6d1f43304b6eb3ad9d16e782
Mano <manokrishnan66@hotmail.com>
2019-10-02 12:28:02 +0530

    importing file

--- Test.R
+++ Test.R
@@ -1,1 +1,13 @@
-sadfasd
+# ---Importing a datafile from dslabs
+
+p <- system.file("extdata", package="dslabs")
+list.files(p)
+
+# -- getting fullpath
+filename <- "murders.csv"
+fullpath <- file.path(p,filename)
+fullpath
+
+#-- copying to working directory
+file.copy(fullpath,getwd())
+file.exists(filename)
