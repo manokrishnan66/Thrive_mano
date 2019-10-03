3c1e9e3340342ad156dcf091ec0230fb95148612
Mano <manokrishnan66@hotmail.com>
2019-10-03 20:45:56 +0530

    Reshaping

--- Test.R
+++ Test.R
@@ -45,3 +45,52 @@ url <- "ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_annmean_mlo.txt"
 co2_mauna_loa <- read_table(url,col_names = TRUE,skip = 56)
 head(co2_mauna_loa)
 nrow(co2_mauna_loa %>% select(year, mean, unc))
+
+#-reshaping test
+d <- read.csv("E:/R_Projects/Thrive_mano/Data/time.csv",stringsAsFactors = FALSE)
+tidy_data <- d %>%
+  gather(year, time, `X2015`:`X2017`)
+tidy_data %>% spread(year, time)
+tidy_data %>% spread(time, year, `X2015`:`X2017`)
+dis <- read.csv("E:/R_Projects/Thrive_mano/Data/disease.csv",stringsAsFactors = FALSE)
+dat_tidy <- dis %>%
+  gather(key = disease, value = count, HepatitisA:Rubella)
+
+dis_m <- read.csv("E:/R_Projects/Thrive_mano/Data/time_merge.csv",stringsAsFactors = FALSE)
+tidy_data_m <- dis_m %>%
+  gather(key = 'key', value = 'value', -age_group) %>%
+  separate(col = key, into = c('year', 'variable_name'), sep = '_') %>% 
+  spread(key = variable_name, value = value)
+
+dis_height <- read.csv("E:/R_Projects/Thrive_mano/Data/height.csv",stringsAsFactors = FALSE)
+tidy_data <- dis_height %>%
+separate(col = key, into = c("player", "variable_name"), sep = "_", extra = "merge")%>%
+spread(key = variable_name, value = value)
+
+co2
+
+co2_wide <- data.frame(matrix(co2, ncol = 12, byrow = TRUE)) %>% 
+  setNames(1:12) %>%
+  mutate(year = as.character(1959:1997))
+
+co2_tidy <- co2_wide %>%
+  gather(month,co2,-year)
+
+co2_tidy %>% ggplot(aes(as.numeric(month), co2, color = year)) + geom_line()
+
+library(dslabs)
+data(admissions)
+dat <- admissions %>% select(-applicants) %>%
+  spread(gender, admitted)
+
+tmp <- gather(admissions, key, value, admitted:applicants)
+tmp
+
+tmp2 <- unite(tmp, column_name, c(key, gender))
+
+tmp2 %>%  spread(column_name, value)
+
+
+
+
+
