247ada5bddd6f196b14a72e54459041864373dae
Mano <manokrishnan66@hotmail.com>
2019-11-09 13:03:33 +0530

    Linear regression

--- Test.R
+++ Test.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(dslabs)
+library(broom)
 install.packages("htmlwidgets")
 install.packages("textdata")
 getwd()
@@ -508,3 +509,403 @@ dim(dat)
     geom_line() +
     geom_vline(xintercept = 20) +
     geom_point() + ylim(0, 110)
+  
+  
+  #------Linear Regression
+  
+  library(Lahman)
+  library(tidyverse)
+  library(dslabs)
+  Teams %>% filter(yearID %in% 1961:2001) %>%
+    mutate(AB_per_game = AB / G, R_per_game = R / G) %>%
+    ggplot(aes(AB_per_game, R_per_game)) + 
+    geom_point(alpha = 0.5)
+  
+  Teams %>% filter(yearID %in% 1961:2001) %>%
+    mutate(X_per_game = X3B / G, Derror_per_game = X2B / G) %>%
+    ggplot(aes(X_per_game, Derror_per_game)) + 
+    geom_point(alpha = 0.5)
+  
+  library(tidyverse)
+  install.packages('HistData')
+  library(HistData)
+  data("GaltonFamilies")
+  data(galton_heights)
+  
+  set.seed(1983)
+  galton_heights <- GaltonFamilies %>%
+    filter(gender == "male") %>%
+    group_by(family) %>%
+    sample_n(1) %>%
+    ungroup() %>%
+    select(father, childHeight) %>%
+    rename(son = childHeight)
+  
+  # means and standard deviations
+  galton_heights %>%
+    summarize(mean(father), sd(father), mean(son), sd(son))
+  # scatterplot of father and son heights
+  galton_heights %>%
+    ggplot(aes(father, son)) +
+    geom_point(alpha = 0.5)
+  
+  B <- 1000
+  N <- 25
+  R <- replicate(B, {
+    sample_n(galton_heights, N, replace = TRUE) %>%
+      summarize(r = cor(father, son)) %>%
+      pull(r)
+  })
+  qplot(R, geom = "histogram", binwidth = 0.05, color = I("black"))
+  
+  # expected value and standard error
+  mean(R)
+  sd(R)
+  
+  B <- 1000
+  N <- 25
+  R <- replicate(B, {
+    sample_n(Teams %>% filter(yearID %in% 1961:2001), N, replace = TRUE) %>%
+      summarize(r = cor(AB / G, R / G)) %>%
+      pull(r)
+  })
+  qplot(R, geom = "histogram", binwidth = 0.05, color = I("black"))
+  
+  # expected value and standard error
+  mean(R)
+  sd(R)
+  
+  
+  Teams_small <- Teams %>% filter(yearID %in% 1961:2001)
+  cor(Teams_small$X2B/Teams_small$G, Teams_small$X3B/Teams_small$G)
+  
+  set.seed(1989) #if you are using R 3.5 or earlier
+  set.seed(1989, sample.kind="Rounding") #if you are using R 3.6 or later
+  library(HistData)
+  data("GaltonFamilies")
+  
+  female_heights <- GaltonFamilies%>%    
+    filter(gender == "female") %>%    
+    group_by(family) %>%    
+    sample_n(1) %>%    
+    ungroup() %>%    
+    select(mother, childHeight) %>%    
+    rename(daughter = childHeight)
+  
+  mu_x <- mean(female_heights$mother)
+  mu_y <- mean(female_heights$daughter)
+  s_x <- sd(female_heights$mother)
+  s_y <- sd(female_heights$daughter)
+  r <- cor(female_heights$mother, female_heights$daughter)
+  m_1 <-  r * s_y / s_x
+  b_1 <- mu_y - m_1*mu_x
+  (m_1*60)+b_1
+  
+  
+  galton_heights %>%
+    mutate(z_father = round((father - mean(father)) / sd(father))) %>%
+    filter(z_father %in% -2:2) %>%
+    ggplot() +  
+    stat_qq(aes(sample = son)) +
+    facet_wrap( ~ z_father)
+  
+  B <- 1000
+  N <- 50
+  lse <- replicate(B, {
+    sample_n(galton_heights, N, replace = TRUE) %>%
+      mutate(father = father - mean(father)) %>%
+      lm(son ~ father, data = .) %>% .$coef 
+  })
+  
+  cor(lse[1,], lse[2,]) 
+  
+  
+  #----Linear Regression
+  
+  set.seed(1983)
+  galton_heights <- GaltonFamilies %>%
+    filter(gender == "male") %>%
+    group_by(family) %>%
+    sample_n(1) %>%
+    ungroup() %>%
+    select(father, childHeight) %>%
+    rename(son = childHeight)
+  rss <- function(beta0, beta1, data){
+    resid <- galton_heights$son - (beta0+beta1*galton_heights$father)
+    return(sum(resid^2))
+  }
+  
+  # plot RSS as a function of beta1 when beta0=25
+  beta1 = seq(0, 1, len=nrow(galton_heights))
+  results <- data.frame(beta1 = beta1,
+                        rss = sapply(beta1, rss, beta0 = 25))
+  results %>% ggplot(aes(beta1, rss)) + geom_line() + 
+    geom_line(aes(beta1, rss))
+  
+  beta1 = seq(0, 1, len=nrow(galton_heights))
+  results <- data.frame(beta1 = beta1,
+                        rss = sapply(beta1, rss, beta0 = 36))
+  results %>% ggplot(aes(beta1, rss)) + geom_line() + 
+    geom_line(aes(beta1, rss), col=2)
+  
+  
+  Teams <- Teams %>% filter(yearID %in% 1961:2001) %>%
+    mutate(BB_per_game = BB / G, R_per_game = R / G, HR_per_game = HR / G)
+    
+  lm (R_per_game~BB_per_game+HR_per_game,data=Teams)%>% .$coef
+  
+  B <- 1000
+  N <- 100
+  lse <- replicate(B, {
+    sample_n(galton_heights, N, replace = TRUE) %>% 
+      lm(son ~ father, data = .) %>% .$coef 
+  })
+  
+  lse <- data.frame(beta_0 = lse[1,], beta_1 = lse[2,]) 
+  
+  set.seed(1989) #if you are using R 3.5 or earlier
+  set.seed(1989, sample.kind="Rounding") #if you are using R 3.6 or later
+  library(HistData)
+  data("GaltonFamilies")
+  options(digits = 3)    # report 3 significant digits
+  female_heights <- GaltonFamilies %>%     
+    filter(gender == "female") %>%     
+    group_by(family) %>%     
+    sample_n(1) %>%     
+    ungroup() %>%     
+    select(mother, childHeight) %>%     
+    rename(daughter = childHeight) 
+  
+  female_heights %>% lm(mother~ daughter, data=.)
+  
+  rss <- function(beta0, beta1, data){
+    resid <- female_heights$mother - (beta0+beta1*female_heights$daughter)
+    return(sum(resid^2))
+  }
+  
+  # plot RSS as a function of beta1 when beta0=25
+  beta1 = seq(0, 1, len=nrow(female_heights))
+  results <- data.frame(beta1 = beta1,
+                        rss = sapply(beta1, rss, beta0 = 25))
+  results %>% ggplot(aes(beta1, rss)) + geom_line() + 
+    geom_line(aes(beta1, rss))
+  #------------------
+  model <- lm(mother ~ daughter, data = female_heights)
+  predictions <- predict(model, interval = c("confidence"), level = 0.95)
+  data <- as.tibble(predictions) %>% bind_cols(daughter = female_heights)
+  
+  ggplot(data, aes(x = father, y = fit)) +
+    geom_line(color = "blue", size = 1) + 
+    geom_ribbon(aes(ymin=lwr, ymax=upr), alpha=0.2) + 
+    geom_point(data = galton_heights, aes(x = father, y = son))
+  
+  
+  
+  library(Lahman)
+  bat_02 <- Batting %>% filter(yearID == 2002) %>%
+    mutate(pa = AB + BB, singles = (H - X2B - X3B - HR)/pa, bb = BB/pa) %>%
+    filter(pa >= 100) %>%
+    select(playerID, singles, bb)
+  
+  bat_99_01 <- Batting %>% filter(yearID %in% 1999:2001) %>%
+    mutate(pa = AB + BB, singles = (H - X2B - X3B - HR)/pa, bb = BB/pa) %>%
+    filter(pa >= 100) %>%
+    group_by(playerID) %>% 
+    summarise(mean_singles = mean(singles), mean_bb = mean(bb))
+  
+  
+  bat_99_01 %>%
+    filter(mean_bb > 0.2) %>%
+    distinct(playerID, mean_singles, mean_bb, .keep_all = FALSE)
+  
+  innerjoin <- bat_02 %>% inner_join(bat_99_01)
+  innerjoin%>% summarise(cor(bb,mean_bb))
+  innerjoin %>% ggplot(aes(singles,mean_singles)) + geom_point()
+  innerjoin %>% ggplot(aes(bb,mean_bb)) + geom_point()
+  
+  get_slope <- function(x, y) cor(x, y) / (sd(x) * sd(y))
+  
+  bb_slope <- innerjoin %>% 
+    summarize(slope = get_slope(mean_singles,singles))
+  
+  
+  innerjoin %>% lm(singles~ mean_singles, data=.)
+  innerjoin %>% lm(bb~ mean_bb, data=.)
+  
+  dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+    mutate(HR = HR/G,
+           R = R/G) %>%
+    select(lgID, HR, BB, R) 
+  
+  dat %>% 
+    group_by(lgID) %>% 
+    do(tidy(lm(R ~ HR, data = .), conf.int = T)) %>% 
+    filter(term == "HR")
+  
+  
+  library(tidyverse)
+  library(HistData)
+  data("GaltonFamilies")
+  set.seed(1) # if you are using R 3.5 or earlier
+  set.seed(1, sample.kind = "Rounding") # if you are using R 3.6 or later
+  galton <- GaltonFamilies %>%
+    group_by(family, gender) %>%
+    sample_n(1) %>%
+    ungroup() %>% 
+    gather(parent, parentHeight, father:mother) %>%
+    mutate(child = ifelse(gender == "female", "daughter", "son")) %>%
+    unite(pair, c("parent", "child"))
+  
+  galton %>%
+    group_by(pair) %>%
+    summarize(cor = cor(parentHeight, childHeight)) %>%
+    filter(cor == min(cor))
+  
+  galton %>%  group_by(pair) %>%
+    do(tidy(lm(childHeight ~ parentHeight, data = .), conf.int = TRUE)) %>%
+    filter(term == "parentHeight") %>%
+    mutate(diff= conf.high - conf.low) %>%
+    select(pair, estimate, conf.low, conf.high, diff)
+  
+  
+  galton %>%
+    group_by(pair) %>%
+    do(tidy(lm(childHeight ~ parentHeight, data = .), conf.int = TRUE)) %>%
+    filter(term == "parentHeight" & p.value < .05)
+  
+  
+  
+  
+  
+  
+  pa_per_game <- Batting %>% 
+    filter(yearID == 2002) %>% 
+    group_by(teamID) %>%
+    summarize(pa_per_game = sum(AB+BB)/max(G)) %>% 
+    .$pa_per_game %>% 
+    mean
+  
+  lm(R ~ 2 + 4 + 1 + 0 + 1)
+  lm(R ~ 1 + 6 + 2 + 1)
+  
+  fit <- Teams %>% filter(yearID==1971) %>% 
+    mutate(BB=BB/G,singles=(H-X2B-X3B-HR)/G,doubles=X2B/G,triples=X3B/G,HR=HR/G,R=R/G) %>% 
+    do(tidy(lm(R~BB+HR, data=.)))
+  
+  Teams %>%
+    filter(yearID == 1971) %>%
+    lm(R ~ BB + HR, data = .) %>%
+    tidy() %>%
+    filter(term == "BB") %>%
+    pull(estimate)
+  
+  res <- Teams %>%
+    filter(yearID %in% 1961:2018) %>%
+    group_by(yearID) %>%
+    do(tidy(lm(R ~ BB + HR, data = .))) %>%
+    ungroup() 
+  res %>%
+    filter(term == "BB") %>%
+    ggplot(aes(yearID, estimate)) +
+    geom_point() +
+    geom_smooth(method = "lm")
+  
+  fit<- Teams %>% +
+    + filter(yearID %in% 1961:2018) %>% +
+    + group_by(yearID) %>% +
+    + do(tidy(lm(R ~ BB + HR, data = .), conf.int = TRUE))
+  
+  fit2 <- fit %>% filter(term == "BB") 
+  fit3 <- lm(estimate ~ yearID, data= fit2)
+  
+  Teams %>%
+    filter(yearID %in% 1961:2018) %>%
+    group_by(yearID) %>%
+    do(tidy(lm(R ~ BB + HR, data = .))) %>%
+    ungroup() %>%
+    filter(term=='BB') %>%
+    lm(estimate ~ yearID, data=.) %>%
+    tidy()
+  
+  Teams %>%
+    filter(yearID == 1971) %>%
+    lm(R ~ BB + HR, data = .) %>%
+    tidy() %>%
+    filter(term == "BB")
+  
+  #--------------------------------
+  library(tidyverse)
+  library(broom)
+  library(Lahman)
+  Teams_small <- Teams %>% 
+    filter(yearID %in% 1961:2001) %>% 
+    mutate(avg_attendance = attendance/G)
+  
+  runs <- Teams_small %>% mutate(rpg = R/G, hrpg = HR/G) %>% lm(avg_attendance ~ hrpg, data = .)
+  
+  tidy(runs)
+  
+  wins <- Teams_small %>% lm(avg_attendance ~ W, data = .)
+  tidy(wins)
+  
+  Year <- Teams_small %>% lm(avg_attendance ~ yearID, data = .)
+  tidy(Year)
+  
+  T <- Teams_small %>% mutate(rpg = R/G, hrpg = HR/G) 
+  
+  T%>% cor(W, rpg, use = "everything")
+  
+  cor(R/G, W)
+  lm(W, R/G, data = Teams_small)
+  
+  cor(Teams_small$W, Teams_small$HR/Teams_small$G)
+  
+  Strata_teams %>%  
+    group_by(Strata) %>% cor(HR/G, avg_attendance)
+  summarize(slope = cor(HR/G, avg_attendance)*sd(avg_attendance)/sd(HR/G))
+  
+  Strata_teams <- Teams_small %>% mutate(Strata = round(W/10,0))%>%
+    group_by(Strata)%>%
+    filter(Strata %in% 5:10) 
+  Strata_teams%>% filter(Strata == 8 )
+  nrow(Strata_teams)
+  
+  
+  dbstrat <- Strata_teams%>%
+    mutate(HRG = HR/G,  RG = R/G)%>%
+    do(tidy(lm(avg_attendance ~ RG, data = .),conf.int = TRUE))%>%
+    filter(term == 'RG')
+  
+  
+  Teams_small <- Teams %>% filter(yearID %in% 1961:2001) %>% 
+    mutate(AT = attendance/G,HRG=HR/G,RG=R/G) %>%
+    select(AT,HRG,W,RG,yearID) %>% 
+    do(tidy(lm(AT~W+HRG+RG+yearID,data=.))) 
+  Teams_small
+  
+  Team_model <- Teams %>% filter(yearID %in% 1961:2001) %>% 
+    mutate(AT = attendance/G,HRG=HR/G,RG=R/G)
+  
+  model <- lm(AT~W+HRG+RG+yearID, data = Team_model)
+  solution_02 <- data.frame(W = 80,HRG = 1.2,RG = 5, yearID = 1960)
+  pred <- predict(model,solution_02)
+  predictions <- predict(model, interval = c("confidence"), level = 0.95)
+  data <- as.tibble(predictions) %>% bind_cols(daughter = female_heights)
+  data %>% summarise(avg=mean(fit))
+  
+  
+  x1=80
+  x2=1.2
+  x3=5
+  x4=2002
+  model$coefficients[1]+(x1*model$coefficients[2])+(x2*model$coefficients[2])+(x3*model$coefficients[3])+(x4*model$coefficients[4])
+  
+  Teams %>% 
+    mutate(AT = attendance/G,HRG=HR/G,RG=R/G) %>%
+    select(AT,HRG,W,RG,yearID) %>% 
+    do(tidy(lm(AT~W+HRG+RG+yearID,data=.))) 
+  
+  Teams_small2002 <- Teams %>% filter(yearID %in% 2002) %>% 
+  mutate(avg_attendance = attendance/G, R_per_game = R/G, HR_per_game = HR/G) 
+  pred_original <- predict(model, Teams_small2002)
+  
