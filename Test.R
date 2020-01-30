799d32d04bcea43ddeda3e5f43858683a135ccb9
Mano <manokrishnan66@hotmail.com>
2020-01-30 19:45:39 +0530

    before capstone

--- Test.R
+++ Test.R
@@ -2168,4 +2168,207 @@ fit_knn9a$bestTune
   oos_test <- rbind.fill(oos_test, data.frame(Sex="male",  Pclass=1, Age=17, SibSp = 2))
   predict(rpart_train, oos_test, type="class")
   
+  fit_rf <- train(Survived ~ ., method = "rf",data = train_set,
+        tuneGrid = data.frame(mtry = seq(1,7,1)), 
+        ntree=100)
+  survived <- predict(fit_rf,test_set)
+  confusionMatrix(data = survived, reference = test_set$Survived)
   
+  varImp(fit_rf)
+  
+  
+  # -----Ensemble-------------------------
+  models <- c("glm", "lda", "naive_bayes", "svmLinear", "knn", "gamLoess", "multinom", "qda", "rf", "adaboost")
+  
+  library(caret)
+  library(dslabs)
+  set.seed(1) # use `set.seed(1, sample.kind = "Rounding")` in R 3.6 or later
+  data("mnist_27")
+  
+  fits <- lapply(models, function(model){ 
+    print(model)
+    train(y ~ ., method = model, data = mnist_27$train)
+  }) 
+  
+  names(fits) <- models
+  
+  models_y_hat <- sapply(fits, function(fit_model){
+    predict(fit_model, mnist_27$test)
+  })
+  dim(models_y_hat)
+  
+  model_accuracies <- colMeans(models_y_hat == mnist_27$test$y)
+  mean(model_accuracies)
+  
+  votes <- rowMeans(models_y_hat == "7")
+  y_hat <- ifelse(votes > 0.5, "7", "2")
+  mean(y_hat == mnist_27$test$y)
+  
+  acc_hat <- sapply(fits, function(fit) min(fit$results$Accuracy))
+  mean(acc_hat)
+  
+  min(fits$results$Accuracy)
+  fits$results$Accuracy
+  
+  df<-as.data.frame(table(models_y_hat[11,]), stringsAsFactors =TRUE)
+  df$Var1
+  
+  y_hat_maj <- sapply(seq(1,nrow(models_y_hat)), function(index_line){
+    df <- as.data.frame(table(models_y_hat[index_line,]))
+    df[which.max(df$Freq),]$Var1
+    df
+  })
+  mean(y_hat_maj == mnist_27$test$y)
+  
+  ind <- acc_hat >= 0.8
+  votes <- rowMeans(models_y_hat[,ind] == "7")
+  y_hat <- ifelse(votes>=0.5, 7, 2)
+  mean(y_hat == mnist_27$test$y)
+  
+  #-----------DImension reduction--------
+  data("tissue_gene_expression")
+  dim(tissue_gene_expression$x)
+  
+  ggplot(aes(tissue_gene_expression$x[,1],tissue_gene_expression$x[,2], color=tissue_gene_expression$y)) + geom_point()
+  
+  pc <- prcomp(tissue_gene_expression$x)
+  
+  data.frame(pc_1 = pc$x[,1], pc_2 = pc$x[,2], 
+             tissue = tissue_gene_expression$y) %>%
+    ggplot(aes(pc_1, pc_2, color = tissue)) +
+    geom_point()
+  
+  bias <- rowMeans(tissue_gene_expression$x)
+  data.frame(pc_1 = pc$x[,1], bias, 
+             tissue = tissue_gene_expression$y) %>%
+    ggplot(aes(pc_1, bias, color = tissue)) +
+    geom_point()
+  
+  cor(pc$x[,1], bias)
+  
+  x <- with(tissue_gene_expression, sweep(x, 1, rowMeans(x)))
+  pc <- prcomp(x)
+  data.frame(pc_1 = pc$x[,1], pc_2 = pc$x[,2], 
+             tissue = tissue_gene_expression$y) %>%
+    ggplot(aes(pc_1, pc_2, color = tissue)) +
+    geom_point()
+  
+  for(i in 1:10){
+    boxplot(pc$x[,i] ~ tissue_gene_expression$y, main = paste("PC", i))
+  }
+  
+  summary(pc)
+  pca_var <- sapply(pc2$sdev[1:10], function(pc_i){
+    pc_i^2 / (t(pc2$sdev) %*% pc2$sdev)  
+  })
+  plot(pca_var) 
+  
+  plot(summary(pc)$importance[3,])
+  
+  library(dslabs)
+  data("movielens")
+  
+  movielens %>% group_by(movieId) %>% 
+         summarize(n = n_distinct(userId),year= as.character(first(year))) %>%
+  
+  qplot(year, n, data = ., geom = "boxplot") +
+    coord_trans(y = "sqrt") +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  
+  
+  movielens %>% filter(year >= 1993 & year <= 2018 ) %>% group_by(movieId) %>% 
+    summarize(avg_rating = mean(rating), n = n(), title=title[1], years=2018 - first(year)) %>%
+    mutate(n_year = n/years) %>%
+    top_n(25, n_year) %>%
+    arrange(desc(n_year))
+  
+    qplot(year, n, data = ., geom = "boxplot") +
+    coord_trans(y = "sqrt") +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  
+  
+    
+    movielens %>% filter(year >= 1993 & year <= 2018 ) %>% group_by(movieId) %>% 
+      summarize(avg_rating = mean(rating), n = n(), title=title[1], years=2018 - first(year)) %>%
+      mutate(n_year = n/years) %>%
+      #top_n(25, n_year) %>%
+      arrange(desc(n_year)) %>%
+    ggplot(aes(n_year,avg_rating)) +
+      geom_point() + geom_smooth()
+    
+    movielens %>%
+      mutate(date = round_date(date, unit = "week")) %>%
+      group_by(date) %>% 
+      summarize(avg_rating = mean(rating)) %>%
+      ggplot(aes(date,avg_rating)) +
+      geom_point() + geom_smooth()
+    
+    
+    movielens %>% mutate(date = round_date(date, unit = "week")) %>%
+      group_by(date) %>%
+      summarize(rating = mean(rating)) %>%
+      ggplot(aes(date, rating)) +
+      geom_point() +
+      geom_smooth()
+    
+    movielens %>% group_by(genres) %>% 
+      summarize(n = n(), avg = mean(rating), se = sd(rating)/sqrt(n())) %>%
+      filter(n >=1000) %>%
+      mutate(genres = reorder(genres, avg)) %>%
+      ggplot(aes(x = genres, y = avg, ymin = avg - 2*se, ymax = avg + 2*se)) + 
+      geom_point() +
+      geom_errorbar() + 
+      theme(axis.text.x = element_text(angle = 90, hjust = 1))
+    
+    
+    
+    set.seed(1986, sample.kind="Rounding")
+    n <- round(2^rnorm(1000, 8, 1))
+    set.seed(1, sample.kind="Rounding")
+    mu <- round(80 + 2*rt(1000, 5))
+    range(mu)
+    schools <- data.frame(id = paste("PS",1:1000),
+                          size = n,
+                          quality = mu,
+                          rank = rank(-mu))
+    schools %>% 
+      top_n(10, quality) %>% 
+      arrange(desc(quality))
+    set.seed(1, sample.kind="Rounding")
+    mu <- round(80 + 2*rt(1000, 5))
+    scores <- sapply(1:nrow(schools), function(i){
+      scores <- rnorm(schools$size[i], schools$quality[i], 30)
+      scores
+    })
+    schools <- schools %>% mutate(score = sapply(scores, mean))
+    head(schools)
+    
+    
+    schools_top10 <- schools %>%
+      top_n(10, score) %>% 
+      arrange(desc(score))
+    schools_top10
+    
+    schools %>% summarise(median(size))
+    schools_top10 %>% summarise(median(size))
+    
+    schools %>% ggplot(aes(size,score)) + geom_point()+
+    geom_point(data = filter(schools, rank<=10), col = 2) 
+    
+    overall <- mean(sapply(scores, mean))
+    alpha <- 25
+    schools5 <- schools %>%
+      mutate(score_dev = overall + (score - overall) * size / (size + alpha)) %>%
+      arrange(desc(score_dev))
+    #    mutate(quality_new = score_dev-80)
+    schools5 %>%
+      top_n(10, score_dev)
+    
+    alpha_best <- 135
+    schools7 <- schools %>%
+      mutate(score_dev = overall + (score - overall) * size / (size + alpha_best)) %>%
+      arrange(desc(score_dev)) %>%
+      top_n(10, score_dev)
+    schools7
+    
+    
